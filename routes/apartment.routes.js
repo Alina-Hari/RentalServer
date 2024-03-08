b2717442c24d9b9f5f7320893405c676c4bd7cf1
Alina Havriushenko <alinagvr@gmail.com>
2024-03-08 11:02:51 +0100

    add images upload

--- routes/apartment.routes.js
+++ routes/apartment.routes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const Apartment = require("../models/Apartment.model");
 const { isAuthenticated } = require("../middleware/jwt.middleware")
+
+const fileUploader = require("../config/cloudinary.config");
+
 router.get("/apartments", (req, res, next) => {
 
   Apartment.find({})
@@ -15,23 +18,23 @@ router.get("/apartments/:apartmentId", (req, res, next) => {
     .catch(err => next(err))
 });
 
-router.post("/apartments",isAuthenticated, (req, res, next) => {
-  const { apartmentType, floor, price, area, isFurnished, isPetFriendly, country,city, availableDates, images } = req.body
-  Apartment.create({ apartmentType, floor, price, area, isFurnished, isPetFriendly, country,city, availableDates, images })
+router.post("/apartments", (req, res, next) => {
+  const { apartmentType, floor, price, area, isFurnished, isPetFriendly, country, city, availableDates, images } = req.body
+  Apartment.create({ apartmentType, floor, price, area, isFurnished, isPetFriendly, country, city, availableDates, images })
     .then(apartment => { res.json(apartment) })
     .catch(err => next(err))
 });
 
 // for agent => add middleware fun
 
-router.put("/apartments/:apartmentId",isAuthenticated, (req, res, next) => {
-  const { apartmentType, floor, price, area, isFurnished, isPetFriendly, country,city, availableDates, isAvailable, images } = req.body
-  Apartment.findByIdAndUpdate(req.params.apartmentId, { apartmentType, floor, price, area, isFurnished, isPetFriendly, country,city, availableDates, isAvailable, images }, { new: true })
+router.put("/apartments/:apartmentId", isAuthenticated, (req, res, next) => {
+  const { apartmentType, floor, price, area, isFurnished, isPetFriendly, country, city, availableDates, isAvailable, images } = req.body
+  Apartment.findByIdAndUpdate(req.params.apartmentId, { apartmentType, floor, price, area, isFurnished, isPetFriendly, country, city, availableDates, isAvailable, images }, { new: true })
     .then(apartment => { res.json(apartment) })
     .catch(err => next(err))
 });
 
-router.delete("/apartments/:apartmentId",isAuthenticated, (req, res, next) => {
+router.delete("/apartments/:apartmentId", isAuthenticated, (req, res, next) => {
 
   Apartment.findByIdAndDelete(req.params.apartmentId)
     .then(res.status(204).send())
@@ -41,7 +44,7 @@ router.delete("/apartments/:apartmentId",isAuthenticated, (req, res, next) => {
 
 // for user => finish editing logic for booking rha apartment
 
-router.put("/apartments/:apartmentId",isAuthenticated, (req, res, next) => {
+router.put("/apartments/:apartmentId", isAuthenticated, (req, res, next) => {
   const { datesBooked, usersBooked, availableDates } = req.body
   Apartment.findByIdAndUpdate(req.params.apartmentId, { datesBooked, usersBooked }, { new: true })
     .then(apartment => { res.json(apartment) })
@@ -59,11 +62,24 @@ router.get("/apartments/location/:locationName", (req, res, next) => {
 
 
 
-router.get("/apartments/:userId",isAuthenticated, (req, res, next) => {
+router.get("/apartments/:userId", isAuthenticated, (req, res, next) => {
   Apartment.find({ usersBooked: req.params.userId }) // check an array
     .then(apartments => { res.json(apartments) })
     .catch(err => next(err))
 });
+
+
+router.post("/upload", fileUploader.array("images", 5), (req, res, next) => {
+
+  if (!req.files || req.files.length === 0) {
+    next(new Error("No files uploaded!"));
+    return;
+  }
+
+  const fileUrls = req.files.map(file => file.path);
+  res.json({ fileUrls });
+});
+
 
 module.exports = router;
 
@@ -72,11 +88,11 @@ module.exports = router;
 // get /apartments X
 // get /apartments/:apartmentId X
 // post /apartments X
-// put /apartments/:apartmentId  -- 
+// put /apartments/:apartmentId  --
 // delete /apartments/:apartmentId X
 
 
 // get appartments/location/ X
-// 
+//
 
 // get /appartments/user/ --
